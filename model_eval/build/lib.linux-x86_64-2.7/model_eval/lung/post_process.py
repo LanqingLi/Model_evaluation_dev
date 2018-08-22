c7430eb0368ed8c975fd9881e89393040a3f0ab8
llanqing <llanqing@infervision.com>
2018-08-22 16:35:04 +0800

    Updated to enable initializion of the hyper-parameters (conf_thresh, fscore_beta, etc.) of BrainSemanticSegEvaluatorOnline by BrainSemanticSegEvaluationOffline

---
+++ model_eval/build/lib.linux-x86_64-2.7/model_eval/lung/post_process.py
@@ -0,0 +1,200 @@
+# -- coding: utf-8 --
+import pandas as pd
+import numpy as np
+from model_eval.lung.config import config
+
+def df_to_cls_label(cls_predict_df_list, cls_gt_df_list, cls_list, thresh=config.TEST.IOU_THRESHOLD):
+    """Convert list of pandas dataframe to list of list of cls labels, as input for custom_metric.ClassificationMetric
+
+    :param cls_predict_df_list:
+    :param cls_gt_df_list:
+    :param cls_list:
+    :return:
+
+    Examples
+    --------
+    predicts = [np.array([1, 1, 1])]
+    labels = [np.array([0, 1, 1])]
+    acc = metric.Accuracy()
+    acc.update(preds = predicts, labels = labels)
+    print acc.get()
+    ('accuracy', 0.666666666666666666)
+    """
+    cls_predict_labels = []
+    cls_gt_labels = []
+    for cls_predict_df, cls_gt_df in zip(cls_predict_df_list, cls_gt_df_list):
+        cls_predict_label = []
+        cls_gt_label = []
+        # No ground truth nodules, all fp
+        if cls_gt_df is None:
+            for index, row in cls_predict_df.iterrows():
+                cls_gt_label.append(0)
+                # print cls_list.index(row['nodule_class'])
+                cls_predict_label.append(cls_list.index(row['nodule_class']))
+        # No predicted nodules, all fn
+        elif cls_predict_df is None:
+            for index, row in cls_gt_df.iterrows():
+                cls_predict_label.append(0)
+
+                # print cls_list.index(row['nodule_class'])
+                cls_gt_label.append(cls_list.index(row['nodule_class']))
+        # non-zero gt and predicted nodules
+        else:
+            pred_nodule = []
+            gt_nodule = []
+
+            # generate interpolated nodule list
+            for i, row_pred in cls_predict_df.iterrows():
+                predict_slices, predict_bboxs = nodule_slice_interpolate_pred(row_pred, z_threshold=1)
+                pred_nodule.append((predict_slices, predict_bboxs))
+
+            for j, row_gt in cls_gt_df.iterrows():
+                gt_slices, gt_bboxs = nodule_slice_interpolate_gt(row_gt)
+                gt_nodule.append((gt_slices, gt_bboxs))
+
+            # construct covariance matrix to account for the equivalence between the gt and predicted nodules
+            cov_matrix = np.zeros([len(cls_gt_df), len(cls_predict_df)])
+            for i, row_gt in cls_gt_df.iterrows():
+                # print cls_list.index(row_pred['nodule_class'])
+                # To account for all samples, we compare each detected nodule with gt, if it doesnt belong to gt, we add a new one and label it as fp
+                # Note that for nodule detection, we didnt account for the background class when run the prediction code, so tn = 0.
+                cls_gt_label.append(cls_list.index(row_gt['nodule_class']))
+                for j, row_pred in cls_predict_df.iterrows():
+                    if nodule_compare(predict_slices=pred_nodule[j][0], ground_truth_slices=gt_nodule[i][0],
+                                      predict_bboxs=pred_nodule[j][1], ground_truth_bboxs=gt_nodule[i][1], thresh=thresh):
+                        cov_matrix[i , j] = 1
+                if np.sum(cov_matrix[i, :]) > 0:
+                    cls_predict_label.append(cls_list.index(row_gt['nodule_class']))
+                else:
+                    cls_predict_label.append(0)
+            for i, row_pred in cls_predict_df.iterrows():
+                if np.sum(cov_matrix[:, i]) == 0:
+                    cls_predict_label.append(cls_list.index(row_pred['nodule_class']))
+                    cls_gt_label.append(0)
+        cls_gt_labels.append(cls_gt_label)
+        cls_predict_labels.append(cls_predict_label)
+
+    return cls_predict_labels, cls_gt_labels
+
+
+
+
+def nodule_slice_interpolate_pred(predict_df_record, z_threshold=1):
+    """
+    :param predict_df_record:一条预测记录,代表一个结节，DataFrame
+
+    :param z_threshold:上下多找多少张
+
+    """
+    predict_slices = range(predict_df_record['slice'][0] - z_threshold,
+                           predict_df_record['slice'][-1] + 1 + z_threshold)
+
+    predict_bboxs = []
+
+    # 补全缺失层面的bbox
+    for predict_slice in predict_slices:
+        if not predict_slice in predict_df_record['slice']:
+            predict_bboxs.append([None, None, None, None])
+        else:
+            i = predict_df_record['slice'].index(predict_slice)
+            predict_bboxs.append(predict_df_record['bbox'][i][:4])
+
+    # 用插值法补全bbox
+    predict_bboxs = np.array(pd.DataFrame(predict_bboxs).interpolate(limit_direction='both'))
+
+
+    return predict_slices, predict_bboxs
+
+def nodule_slice_interpolate_gt(ground_truth_df_record):
+    """
+    :param ground_truth_df_record:一条ground truth记录，代表一个结节，DataFrame
+    """
+    ground_truth_slices = range(ground_truth_df_record['slice'][0],
+                                ground_truth_df_record['slice'][-1] + 1)
+
+    ground_truth_bboxs = []
+
+    # 补全缺失层面的bbox
+    for ground_truth_slice in ground_truth_slices:
+        if not ground_truth_slice in ground_truth_df_record['slice']:
+            ground_truth_bboxs.append([None, None, None, None])
+        else:
+            i = ground_truth_df_record['slice'].index(ground_truth_slice)
+            ground_truth_bboxs.append(ground_truth_df_record['bbox'][i][:4])
+
+    # 用插值法补全bbox
+    ground_truth_bboxs = np.array(pd.DataFrame(ground_truth_bboxs).interpolate(limit_direction='both'))
+
+    return ground_truth_slices, ground_truth_bboxs
+
+def nodule_compare(predict_slices, ground_truth_slices, predict_bboxs, ground_truth_bboxs, thresh = config.TEST.IOU_THRESHOLD):
+    """
+    将一个预测结节和一个实际结节比较，给出是否匹配
+    :param predict_df_record:一条预测记录,代表一个结节，DataFrame
+    :param ground_truth_df_record:一条ground truth记录，代表一个结节，DataFrame
+    :param z_threshold:上下多找多少张
+    :return:比较后的的结果，bool
+    """
+    # 计算交叉层面bbox的DICE
+    cross_slices = list(set(predict_slices) & set(ground_truth_slices))
+    # 无交叉返回0
+    if len(cross_slices) == 0:
+        return False
+    # 有交叉，只要有一对满足标准，就算对
+    ret_if_same_bbox = False
+    for slice in cross_slices:
+        predict_index = predict_slices.index(slice)
+        ground_truth_index = ground_truth_slices.index(slice)
+        # score = find_nodules.calcDICE(predict_bboxs[predict_index], ground_truth_bboxs[ground_truth_index])
+        if_same_bbox = cal_same_bbox(ground_truth_bboxs[ground_truth_index], predict_bboxs[predict_index], thresh=thresh)
+        # 只要有一对满足标准，就算对
+        ret_if_same_bbox = ret_if_same_bbox or if_same_bbox
+        if ret_if_same_bbox:
+            break
+    return ret_if_same_bbox
+
+
+def cal_same_bbox(bbox_gt, bbox_pt, thresh = config.TEST.IOU_THRESHOLD):
+    return IOU(bbox_gt, bbox_pt) > thresh
+
+def overlapND(x, y):
+    """
+    :params x: 2*n np array
+    :params y: 2*n np array
+    """
+    assert (x.shape[0] == 2)
+    assert (y.shape[0] == 2)
+    res = []
+    for i in range(x.shape[1]):
+        res.append(overlap1D(x[:, i], y[:, i]))
+    return np.vstack(res).T
+
+def overlap1D(x, y):
+    """
+    Returns the overlap of 1d segment, returns [0, 0] if not overlapped.
+    :params x: 1d np array of 2 elements. [st, ed]
+    :params y: 1d np array of 2 elements. [st ,ed]
+    """
+    st = np.max([x[0], y[0]])
+    ed = np.min([x[1], y[1]])
+    return np.array([st, ed])
+
+def IOU(box1, box2):
+    """
+
+    :param box1:
+    :param box2:
+    :return:
+    Intersection over union (IOU) between two bounding boxes
+    """
+    box1 = box1.copy().reshape([2, 2])
+    box2 = box2.copy().reshape([2, 2])
+    if np.any(box1[1] - box1[0] <= 0) or np.any(box2[1] - box2[0] <= 0):
+        raise ValueError(
+            "Boxes should be represented as [xmin, ymin, xmax, ymax]. Box1: %s. Box2: %s. " % (str(box1), str(box2)))
+
+    res = overlapND(box1, box2)
+    # return res
+    if np.any(res[1] - res[0] <= 0):
+        return 0.0
+    return 2 * float(np.prod(res[1] - res[0])) / (np.prod(box1[1] - box1[0]) + np.prod(box2[1] - box2[0]) - np.prod(res[1] - res[0]))
---
+++ model_eval/build/lib.linux-x86_64-2.7/model_eval/lung/xml_tools.py
@@ -0,0 +1,347 @@
+# ------ coding: utf-8 --------
+import os
+import xml.etree.cElementTree as ET
+from model_eval.lung.config import config
+
+
+# 输入一个xml文件(某个slice), 输出[[bbx1],[bbx2],[bbx3]]
+# ['nodule', '0-3nodule', '3-6nodule', '6-10nodule',
+#                     'calcific nodule', 'pgo', '10-30nodule', 'mass',
+#                     '0-5GGN', '5GGN', 'calcific',
+#                     'pleural nodule', 'quasi-nodule']
+def read_xml(xml_path, restrict_name_list=None):
+    '''
+    读取xml
+    :param xml_path: xml文件的路径
+    :param restrict_name_list: 结节种类表（list），一般为config.CLASSES
+    :return:boxes的list，每个box [xmin,ymin,xmax,ymax,概率（gt为1）,结节种类]
+    '''
+    tree = ET.parse(xml_path)
+    objs = tree.findall('object')
+    boxes = []
+    for ix, obj in enumerate(objs):
+        name = obj.find('name').text
+        if restrict_name_list == None:
+            restrict_name_list = ['nodule', '0-3nodule', '3-6nodule', '6-10nodule',
+                                  'calcific nodule', 'pgo', '10-30nodule', 'mass',
+                                  '0-5GGN', '5GGN', 'calcific',
+                                  'pleural nodule', 'quasi-nodule']
+        try:
+            mapped_name = config.CLASS_DICT[name]
+            if mapped_name in restrict_name_list:
+                # ['0-5GGN', '5GGN']:
+                bbox = obj.find('bndbox')
+                x1 = int(bbox.find('xmin').text)
+                y1 = int(bbox.find('ymin').text)
+                x2 = int(bbox.find('xmax').text)
+                y2 = int(bbox.find('ymax').text)
+                box = [x1, y1, x2, y2, 1, mapped_name]
+                boxes.append(box)
+        except:
+            pass
+    return boxes
+
+def read_xml_without_nodule_cls(xml_path, restrict_name_list=None):
+    '''
+    读取xml
+    :param xml_path: xml文件的路径
+    :param restrict_name_list: 结节种类表（list），一般为config.CLASSES
+    :return:boxes的list，每个box [xmin,ymin,xmax,ymax,概率（gt为1）,结节种类]
+    '''
+    # 如果用不了请联系ylifeng帮你改
+    tree = ET.parse(xml_path)
+    objs = tree.findall('object')
+    boxes = []
+    for ix, obj in enumerate(objs):
+        # no nodule class info in anno, all set to nodule by default
+        name = '0-3nodule'
+        if restrict_name_list == None:
+            restrict_name_list = ['nodule', '0-3nodule', '3-6nodule', '6-10nodule',
+                                  'calcific nodule', 'pgo', '10-30nodule', 'mass',
+                                  '0-5GGN', '5GGN', 'calcific',
+                                  'pleural nodule', 'quasi-nodule']
+        try:
+            mapped_name = config.CLASS_DICT[name]
+            if mapped_name in restrict_name_list:
+                # ['0-5GGN', '5GGN']:
+                bbox = obj.find('bndbox')
+                x1 = int(bbox.find('xmin').text)
+                y1 = int(bbox.find('ymin').text)
+                x2 = int(bbox.find('xmax').text)
+                y2 = int(bbox.find('ymax').text)
+                box = [x1, y1, x2, y2, 1., name]
+                boxes.append(box)
+        except:
+            pass
+    return boxes
+
+def read_xml_with_nodule_num(xml_path, xml_name, restrict_name_list=None):
+    '''
+    读取xml
+    :param xml_path: xml文件的路径
+    :param restrict_name_list: 结节种类表（list），一般为config.CLASSES
+    :return:boxes的list，每个box [xmin,ymin,xmax,ymax,概率（gt为1）,结节种类,结节编号，层面数]
+    '''
+    # 如果用不了请联系ylifeng帮你改
+    tree = ET.parse(xml_path)
+    objs = tree.findall('object')
+    boxes = []
+    slice_id = int(xml_name.split("_")[-1].split(".")[0]) - 1
+    for ix, obj in enumerate(objs):
+        name = obj.find('name').text
+        nodule_num = int(obj.find('lobe_pos').text)
+        if restrict_name_list == None:
+            restrict_name_list = ['nodule', '0-3nodule', '3-6nodule', '6-10nodule',
+                                  'calcific nodule', 'pgo', '10-30nodule', 'mass',
+                                  '0-5GGN', '5GGN', 'calcific',
+                                  'pleural nodule', 'quasi-nodule']
+        try:
+            mapped_name = config.CLASS_DICT[name]
+            if mapped_name in restrict_name_list:
+                # ['0-5GGN', '5GGN']:
+                bbox = obj.find('bndbox')
+                x1 = int(bbox.find('xmin').text)
+                y1 = int(bbox.find('ymin').text)
+                x2 = int(bbox.find('xmax').text)
+                y2 = int(bbox.find('ymax').text)
+                box = [x1, y1, x2, y2, 1, mapped_name, nodule_num, slice_id]
+                boxes.append(box)
+        except:
+            pass
+    return boxes
+
+def read_xml_with_nodule_num_without_nodule_cls(xml_path, xml_name, restrict_name_list=None):
+    '''
+    读取xml
+    :param xml_path: xml文件的路径
+    :param restrict_name_list: 结节种类表（list），一般为config.CLASSES
+    :return:boxes的list，每个box [xmin,ymin,xmax,ymax,概率（gt为1）,结节种类,结节编号，层面数]
+    '''
+    tree = ET.parse(xml_path)
+    objs = tree.findall('object')
+    boxes = []
+    slice_id = int(xml_name.split("_")[-1].split(".")[0]) - 1
+    for ix, obj in enumerate(objs):
+        name = '0-3nodule'
+        nodule_num = int(obj.find('lobe_pos').text)
+        if restrict_name_list == None:
+            restrict_name_list = ['nodule', '0-3nodule', '3-6nodule', '6-10nodule',
+                                  'calcific nodule', 'pgo', '10-30nodule', 'mass',
+                                  '0-5GGN', '5GGN', 'calcific',
+                                  'pleural nodule', 'quasi-nodule']
+        try:
+            mapped_name = config.CLASS_DICT[name]
+            if mapped_name in restrict_name_list:
+                # ['0-5GGN', '5GGN']:
+                bbox = obj.find('bndbox')
+                x1 = int(bbox.find('xmin').text)
+                y1 = int(bbox.find('ymin').text)
+                x2 = int(bbox.find('xmax').text)
+                y2 = int(bbox.find('ymax').text)
+                box = [x1, y1, x2, y2, 1, mapped_name, nodule_num, slice_id]
+                boxes.append(box)
+        except:
+            pass
+    return boxes
+
+
+
+def read_xml_for_multi_classes(xml_path, restrict_name_list=None):
+    '''
+    读取xml
+    :param xml_path: xml文件的路径
+    :param restrict_name_list: 结节种类表（list），一般为config.CLASSES
+    :return:boxes的list，每个box [xmin,ymin,xmax,ymax,概率（gt为1）,结节种类]
+    '''
+    tree = ET.parse(xml_path)
+    objs = tree.findall('object')
+    boxes = []
+    for ix, obj in enumerate(objs):
+        name = obj.find('name').text
+        if restrict_name_list == None:
+            restrict_name_list = ['0-3nodule', '3-6nodule', '6-10nodule',
+                                  'calcific nodule','10-30nodule', 'mass',
+                                  '0-5GGN', '5GGN','pleural nodule']
+        try:
+            if name in restrict_name_list:
+                # ['0-5GGN', '5GGN']:
+                bbox = obj.find('bndbox')
+                x1 = int(bbox.find('xmin').text)
+                y1 = int(bbox.find('ymin').text)
+                x2 = int(bbox.find('xmax').text)
+                y2 = int(bbox.find('ymax').text)
+                box = [x1, y1, x2, y2, 1, name]
+                boxes.append(box)
+        except:
+            pass
+    return boxes
+
+
+def generate_xml(data_dir, filename, name_bboxs_dict):
+    root = ET.Element("annotation")
+    filename_doc = ET.SubElement(root, "filename")
+    filename_doc.text = filename
+    for index, row in name_bboxs_dict.iterrows():
+        object_doc = ET.SubElement(root, "object")
+        ET.SubElement(object_doc, "name").text = row['nodule_class']
+        bndbox_doc = ET.SubElement(object_doc, "bndbox")
+        ET.SubElement(bndbox_doc, "xmin").text = str(int(round(row['xmin'])))
+        ET.SubElement(bndbox_doc, "ymin").text = str(int(round(row['ymin'])))
+        ET.SubElement(bndbox_doc, "xmax").text = str(int(round(row['xmax'])))
+        ET.SubElement(bndbox_doc, "ymax").text = str(int(round(row['ymax'])))
+        ET.SubElement(bndbox_doc, "prob").text = str(row['prob'])
+    tree = ET.ElementTree(root)
+    tree.write(os.path.join(data_dir, filename))
+
+
+def xml_to_boxeslist_multi_classes(xml_dir, box_length):
+    '''
+    读取文件夹下的xml，返回和get_boxes返回格式一样的boxes
+    :param xml_dir:
+    :param box_length:
+    :return: box list 一个四层的list,第一层id为层面号,第二层id为结节种类，第三层id为bndbox_id,第四层为[xmin,ymin,xmax,ymax,prob]
+    例如，box_list[123][1][3] = 表示的是第124层的，大肿块（加入2号类为大肿块）中，的第四个大肿块，的bndbox信息
+    '''
+    return_boxes_list = []
+    num_classes = len(config.NODULE_CLASSES)  # CLASSES有background, 所以要+1
+    # 初始化空列表
+    for i_row in range(box_length):
+        slice_boxes = []
+        for i_cls in range(num_classes):
+            slice_boxes.append([])
+        return_boxes_list.append(slice_boxes)
+    if xml_dir != None:
+        # 读取xml，找到对应层面，存入对应种类的list
+        xml_names = os.listdir(xml_dir)
+        for xml_name in xml_names:
+            slice_id = int(xml_name.split("_")[-1].split(".")[0]) - 1
+            xml_path = xml_dir + "/" + xml_name
+            boxes = read_xml_for_multi_classes(xml_path,config.NODULE_CLASSES)
+            for box in boxes:
+                name = box[-1]
+                id_cls = config.NODULE_CLASSES.index(name)
+                return_boxes_list[slice_id][id_cls].append(box[:-1])
+    return return_boxes_list
+
+def xml_to_boxeslist(xml_dir, box_length):
+    '''
+    读取文件夹下的xml，返回和get_boxes返回格式一样的boxes
+    :param xml_dir:
+    :param box_length:
+    :return: box list 一个四层的list,第一层id为层面号,第二层id为结节种类，第三层id为bndbox_id,第四层为[xmin,ymin,xmax,ymax,prob]
+    例如，box_list[123][1][3] = 表示的是第124层的，大肿块（加入2号类为大肿块）中，的第四个大肿块，的bndbox信息
+    '''
+    return_boxes_list = []
+    num_classes = len(config.CLASSES)  # CLASSES有background, 所以要-1
+    # 初始化空列表
+    for i_row in range(box_length):
+        slice_boxes = []
+        for i_cls in range(num_classes - 1):
+            slice_boxes.append([])
+        return_boxes_list.append(slice_boxes)
+    if xml_dir != None:
+        # 读取xml，找到对应层面，存入对应种类的list
+        xml_names = os.listdir(xml_dir)
+        for xml_name in xml_names:
+            slice_id = int(xml_name.split("_")[-1].split(".")[0]) - 1
+            xml_path = xml_dir + "/" + xml_name
+            boxes = read_xml(xml_path, config.CLASSES)
+            for box in boxes:
+                name = box[-1]
+                id_cls = config.CLASSES.index(name) - 1
+                return_boxes_list[slice_id][id_cls].append(box[:-1])
+    return return_boxes_list
+
+def xml_to_boxeslist_without_nodule_cls(xml_dir, box_length):
+    '''
+    读取文件夹下的xml，返回和get_boxes返回格式一样的boxes
+    :param xml_dir:
+    :param box_length:
+    :return: box list 一个四层的list,第一层id为层面号,第二层id为结节种类，第三层id为bndbox_id,第四层为[xmin,ymin,xmax,ymax,prob]
+    例如，box_list[123][1][3] = 表示的是第124层的，大肿块（加入2号类为大肿块）中，的第四个大肿块，的bndbox信息
+    '''
+    return_boxes_list = []
+    num_classes = len(config.CLASSES)  # CLASSES有background, 所以要-1
+    # 初始化空列表
+    for i_row in range(box_length):
+        slice_boxes = []
+        for i_cls in range(num_classes - 1):
+            slice_boxes.append([])
+        return_boxes_list.append(slice_boxes)
+    if xml_dir != None:
+        # 读取xml，找到对应层面，存入对应种类的list
+        xml_names = os.listdir(xml_dir)
+        for xml_name in xml_names:
+            #print xml_name
+            slice_id = int(xml_name.split("_")[-1].split(".")[0]) - 1
+            xml_path = xml_dir + "/" + xml_name
+            boxes = read_xml_without_nodule_cls(xml_path, config.CLASSES)
+            for box in boxes:
+                name = box[-1]
+                id_cls = config.CLASSES.index(name) - 1
+                return_boxes_list[slice_id][id_cls].append(box[:-1])
+    return return_boxes_list
+
+def xml_to_boxeslist_with_nodule_num(xml_dir, box_length):
+    '''
+    读取文件夹下的xml，返回和get_boxes返回格式一样的boxes
+    :param xml_dir:
+    :param box_length:
+    :return: box list 一个四层的list,第一层id为层面号,第二层id为结节种类，第三层id为bndbox_id,第四层为[xmin,ymin,xmax,ymax,prob,nodule_num,sliceId]
+    例如，box_list[123][1][3] = 表示的是第124层的，大肿块（加入2号类为大肿块）中，的第四个大肿块，的bndbox信息
+    '''
+    return_boxes_list = []
+    box_list_all_slice = []
+    num_classes = len(config.CLASSES)  # CLASSES有background, 所以要-1
+    # 初始化空列表
+    for i_row in range(box_length):
+        slice_boxes = []
+        for i_cls in range(num_classes - 1):
+            slice_boxes.append([])
+        return_boxes_list.append(slice_boxes)
+    if xml_dir != None:
+        # 读取xml，找到对应层面，存入对应种类的list
+        xml_names = os.listdir(xml_dir)
+        for xml_name in xml_names:
+            slice_id = int(xml_name.split("_")[-1].split(".")[0]) - 1
+            xml_path = xml_dir + "/" + xml_name
+            # nodule_num = boxes[6], nodule_class = boxes[5]
+            boxes = read_xml_with_nodule_num(xml_path, xml_name, config.CLASSES)
+            for box in boxes:
+                name = box[-3]
+                id_cls = config.CLASSES.index(name) - 1
+                return_boxes_list[slice_id][id_cls].append(box[:5])
+                box_list_all_slice.append(box)
+    return return_boxes_list, box_list_all_slice
+
+def xml_to_boxeslist_with_nodule_num_without_nodule_cls(xml_dir, box_length):
+    '''
+    读取文件夹下的xml，返回和get_boxes返回格式一样的boxes
+    :param xml_dir:
+    :param box_length:
+    :return: box list 一个四层的list,第一层id为层面号,第二层id为结节种类，第三层id为bndbox_id,第四层为[xmin,ymin,xmax,ymax,prob,nodule_num,sliceId]
+    例如，box_list[123][1][3] = 表示的是第124层的，大肿块（加入2号类为大肿块）中，的第四个大肿块，的bndbox信息
+    '''
+    return_boxes_list = []
+    box_list_all_slice = []
+    num_classes = len(config.CLASSES)  # CLASSES有background, 所以要-1
+    # 初始化空列表
+    for i_row in range(box_length):
+        slice_boxes = []
+        for i_cls in range(num_classes - 1):
+            slice_boxes.append([])
+        return_boxes_list.append(slice_boxes)
+    if xml_dir != None:
+        # 读取xml，找到对应层面，存入对应种类的list
+        xml_names = os.listdir(xml_dir)
+        for xml_name in xml_names:
+            slice_id = int(xml_name.split("_")[-1].split(".")[0]) - 1
+            xml_path = xml_dir + "/" + xml_name
+            # nodule_num = boxes[6], nodule_class = boxes[5]
+            boxes = read_xml_with_nodule_num_without_nodule_cls(xml_path, xml_name, config.CLASSES)
+            for box in boxes:
+                name = box[-3]
+                id_cls = config.CLASSES.index(name) - 1
+                return_boxes_list[slice_id][id_cls].append(box[:5])
+                box_list_all_slice.append(box)
+    return return_boxes_list, box_list_all_slice
